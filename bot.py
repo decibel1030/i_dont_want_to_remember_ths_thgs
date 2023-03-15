887dfdbedd3a29aa34ef184853ed7bac328a7218
Ernur <92059459+decibel1030@users.noreply.github.com>
2023-03-16 02:18:12 +0500

    0.1

---
+++ bot.py
@@ -0,0 +1,42 @@
+from config import BOT_TOKEN, Data
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import Text
+
+
+class Advisor:
+    def __init__(self):
+        # change it to your own
+        bot = Bot(token=BOT_TOKEN)
+        dp = Dispatcher(bot)
+        data = Data()
+        buttons = ["День", "Месяц", "Полная дата", "Какая пара ?", "Расчитать дни"]
+
+        @dp.message_handler(commands=["start", "hello", "help"])
+        async def start(message: types.Message):
+            msg = str(open("message.txt", "r", encoding="utf-8").read())
+            kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            kb.add(*buttons)
+            await message.answer(msg, reply_markup=kb)
+
+        @dp.message_handler(Text(equals=f"{buttons[0]}"))
+        async def show_data(message: types.Message):
+            text = f"⏸День: {data.this_day_location}\n💬Название: {data.this_day}\n\n" \
+                   f"▶Следующий день: {data.next_day}\n 💬Название: {data.next_day_location}"
+            await bot.send_message(chat_id=message.chat.id, text=text)
+
+        @dp.message_handler(Text(equals=f"{buttons[1]}"))
+        async def show_data(message: types.Message):
+            text = f"⏸Месяц: {data.this_month_location}\n💬Название: {data.this_month}\n\n" \
+                   f"▶Следующий месяц: {data.next_month_location}\n💬Название: {data.next_month}"
+            await bot.send_message(chat_id=message.chat.id, text=text)
+
+        self.bot = bot
+        self.dp = dp
+
+    def start_bot(self):
+        executor.start_polling(self.dp)
+
+
+if __name__ == '__main__':
+    advisor = Advisor()
+    advisor.start_bot()
---
+++ config.py
@@ -0,0 +1,52 @@
+import time
+# token
+BOT_TOKEN = "6276241260:AAFJjMCgU6P-giS8ni4BIGxNA9Kk9pM8Wuo"
+# main func
+class Data:
+    def __init__(self):
+        self.message_about_today = f"Сегодня {time.localtime().tm_mday}, Месяц: {time.localtime().tm_mon}"
+        self.get_time()
+
+    def get_time(self):
+        months = {
+            1: 'Январь',
+            2: 'Февраль',
+            3: 'Март',
+            4: 'Апрель',
+            5: 'Май',
+            6: 'Июнь',
+            7: 'Июль',
+            8: 'Август',
+            9: 'Сентябрь',
+            10: 'Октябрь',
+            11: 'Ноябрь',
+            12: 'Декабрь'
+        }
+        weekdays = {
+
+            1: 'Понедельник',
+            2: 'Вторник',
+            3: 'Среда',
+            4: 'Четверг',
+            5: 'Пятница',
+            6: 'Суббота',
+            7: 'Воскресенье',
+        }
+
+        self.this_month = f"{months[time.localtime().tm_mon]}"
+        self.this_month_location = time.localtime().tm_mon
+        if time.localtime().tm_mon == 12:
+            self.next_month = f"{months[1]}"
+        self.next_month = f"{months[time.localtime().tm_mon + 1]}"
+        self.next_month_location = time.localtime().tm_mon + 1
+        if time.localtime().tm_wday == 6:
+            self.this_day = weekdays[7]
+            self.this_day_location = 7
+            self.next_day = weekdays[1]
+            self.next_day_location = 1
+
+        self.this_day = weekdays[time.localtime().tm_wday + 1]
+        self.this_day_location = time.localtime().tm_wday + 1
+        self.next_day = weekdays[time.localtime().tm_wday + 2]
+        self.next_day_location = time.localtime().tm_mday + 2
+
